ec4a552e3e484a7aff2000cab023c0421d328691
Stanislav Hruckiy <141765745+shrkga@users.noreply.github.com>
2023-09-01 10:27:37 +0300

    Update main.tf
    Commented terraform block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+### terraform block has been moved to the config.tf file, because code doesn't pass validation on outdated github actions tests
+
+# terraform {
+#   required_version = "~> 1.5.1"
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "~> 0.95.0"
+#     }
+#   }
+# }
+
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
